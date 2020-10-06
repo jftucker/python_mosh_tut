17c52601e1fe739af68eeb6b985d84d733796187
john <johntucker0686@gmail.com>
2020-10-06 08:31:43 -0700

    add demonstrations of std library

--- moshpdf/moshpdf/pdf2text.py
+++ moshpdf/moshpdf/pdf2text.py
@@ -1,12 +1,18 @@
 """ This module provides functions to convert a PDF to text."""
-def convert(path):
-  """
-  Convert the given PDF to text.
 
-  Parameters:
-  path (str): The path to a PDF file.
+class Converter:
+  """ A simple converter for converting PDFs to text. """
+  
+  def convert(self, path):
+    """
+    Convert the given PDF to text.
 
-  Returns:
-  str: The content of the PDF file as text.
-  """
-  print("pdf2text")
+    Parameters:
+    path (str): The path to a PDF file.
+
+    Returns:
+    str: The content of the PDF file as text.
+    """
+    print("pdf2text")
+
+convert()
